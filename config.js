fdb1b78a66061b02ff9f1793fc68bdcadbe0600b
pythagore23 <lpythagore7@gmail.com>
2025-07-16 22:05:46 +0100

    config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
     CHATBOT: getConfig("CHATBOT") || "on",
     //chatbot on/off
   
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON-MD~",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~cNx3BYKK#MpwCmiz7OJ19IB697cuuoMkI5UxiNhc-Dq5Nh6IHivk",  // Your bot's session ID (keep it secure)
     
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     
